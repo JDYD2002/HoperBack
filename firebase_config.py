a758f5d1fc4aa7122eb1fd2be17d99952d93f1bd
Felipe Braga <ydjd2002@gmail.com>
2025-08-26 16:00:09 -0300

    Update firebase_config.py

--- firebase_config.py
+++ firebase_config.py
@@ -1,8 +1,11 @@
+import os
+import json
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-# Caminho para sua chave JSON do Firebase
-cred = credentials.Certificate("chave_firebase.json")
+cred_json = os.getenv("FIREBASE_CRED_JSON")
+cred_dict = json.loads(cred_json)
+cred = credentials.Certificate(cred_dict)
 firebase_admin.initialize_app(cred)
 
 db_firebase = firestore.client()
