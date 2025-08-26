8a5ea37cc92f1ed3bc34551aad674e984c2277e1
Felipe Braga <ydjd2002@gmail.com>
2025-08-26 17:13:44 -0300

    Update firebase_config.py

--- firebase_config.py
+++ firebase_config.py
@@ -4,6 +4,10 @@ from firebase_admin import credentials, firestore
 
 cred_json = os.getenv("FIREBASE_CRED_JSON")
 cred_dict = json.loads(cred_json)
+
+# Substitui os "\n" escapados por quebras de linha reais
+cred_dict["private_key"] = cred_dict["private_key"].replace("\\n", "\n")
+
 cred = credentials.Certificate(cred_dict)
 firebase_admin.initialize_app(cred)
 
