b1b8c7139a4c3723f3c889ac040973dbe0e8d660
Felipe Braga <ydjd2002@gmail.com>
2025-08-26 16:24:10 -0300

    Update firebase_config.py

--- firebase_config.py
+++ firebase_config.py
@@ -1,10 +1,9 @@
-import os
-import json
-import firebase_admin
+import os, json
 from firebase_admin import credentials, firestore
+import firebase_admin
 
 cred_json = os.getenv("FIREBASE_CRED_JSON")
-cred_dict = json.loads(cred_json)
+cred_dict = json.loads(cred_json)  # Aqui não deve dar erro
 cred = credentials.Certificate(cred_dict)
 firebase_admin.initialize_app(cred)
 
